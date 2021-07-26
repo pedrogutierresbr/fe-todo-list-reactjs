08f99abd417ae12d7f47137a5510e4db1375962b
Pedro Gutierres <pedro-gabriel1@hotmail.com>
2021-07-26 11:58:17 -0300

    criado Modal para concluir tarefa e funcao que vai mudar a tarefa.concluido de falso para verdadeiro, assim aplicando css de line-through

--- src/components/listar/concluir-tarefa/Concluir-tarefa.js
+++ src/components/listar/concluir-tarefa/Concluir-tarefa.js
@@ -5,8 +5,30 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClipboardCheck } from "@fortawesome/free-solid-svg-icons";
 
 function ConcluirTarefa(props) {
+    const [exibirModal, setExibirModal] = useState(false);
+
     function handleAbrirModal(event) {
         event.preventDefault();
+        setExibirModal(true);
+    }
+
+    function handleFecharModal() {
+        setExibirModal(false);
+    }
+
+    function handleConcluirTarefa(event) {
+        event.preventDefault();
+        const tarefasDB = localStorage["tarefas"];
+        let tarefas = tarefasDB ? JSON.parse(tarefasDB) : [];
+        tarefas = tarefas.map((tarefa) => {
+            if (tarefa.id === props.tarefa.id) {
+                tarefa.concluida = true;
+            }
+            return tarefa;
+        });
+        localStorage["tarefas"] = JSON.stringify(tarefas);
+        setExibirModal(false);
+        props.recarregarTarefas(true);
     }
 
     return (
@@ -14,6 +36,28 @@ function ConcluirTarefa(props) {
             <Button className="btn-sm" onClick={handleAbrirModal} data-testid="btn-abrir-modal">
                 <FontAwesomeIcon icon={faClipboardCheck} />
             </Button>
+
+            <Modal show={exibirModal} onHide={handleFecharModal} data-testid="modal">
+                <Modal.Header closeButton>
+                    <Modal.Title>Concluir tarefa</Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body>
+                    Deseja realmente concluir a seguinte tarefa?
+                    <br />
+                    <strong>{props.tarefas.nome}</strong>
+                </Modal.Body>
+
+                <Modal.Footer>
+                    <Button variant="primary" onClick={handleConcluirTarefa} data-testid="btn-concluir">
+                        Sim
+                    </Button>
+
+                    <Button varian="light" onClick={handleFecharModal} data-testid="btn-fechar-modal">
+                        Não
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </span>
     );
 }

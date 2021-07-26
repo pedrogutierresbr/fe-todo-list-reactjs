720f007d08913592c1d0b785c637a5cd6d82b447
Pedro Gutierres <pedro-gabriel1@hotmail.com>
2021-07-26 09:44:22 -0300

    criado layout inicial  da tabela que listará as tarefas

--- src/components/Listar-tarefas/Listar-tarefa.js
+++ src/components/Listar-tarefas/Listar-tarefa.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { A } from "hookrouter";
+import { Table } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 function ListarTarefas() {
     return (
-        <A href="/cadastrar" className="btn btn-success btn-sm">
-            Nova tarefa
-        </A>
+        <div className="text-center container mt-5">
+            <h3>Tarefas a fazer</h3>
+            <Table className="mt-3" striped bordered hover responsive data-testid="table">
+                <thead>
+                    <tr>
+                        <th>Tarefa</th>
+                        <th>
+                            <A href="/cadastrar" className="btn btn-success btn-sm" data-testid="btn-nova-tarefa">
+                                <FontAwesomeIcon className="mr-2" icon={faPlus} />
+                                Nova tarefa
+                            </A>
+                        </th>
+                    </tr>
+                </thead>
+
+                <tbody></tbody>
+            </Table>
+        </div>
     );
 }
 
